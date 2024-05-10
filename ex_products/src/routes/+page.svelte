3cc21aa18e25aad37922d471898aec10f4c13ed6
typescript-team <type.script@email.com>
2024-05-10 19:19:23 +0900

    commit

--- ex_products/src/routes/+page.svelte
+++ ex_products/src/routes/+page.svelte
@@ -1,1 +1,6 @@
+<script lang="ts">
+</script>
+
+<!-- https://github.com/sveltekits/HelloSvelte/blob/code/src/routes/shop -->
+
 <h1>Welcome to SvelteKit</h1>
---
+++ ex_products/src/routes/product/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { PageServerData } from './$types';
+	export let data: PageServerData;
+</script>
+
+<svelte:head>
+	<title>즐거운 쇼핑</title>
+</svelte:head>
+
+<h1>상품 목록</h1>
+{#each data.products as product}
+	<h2>목록</h2>
+{/each}
